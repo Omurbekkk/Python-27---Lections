84e6962bcfa551be73b1b6b2c14f669ee1403653
Omurbek <odosumbekov@gmail.com>
2023-02-02 12:42:18 +0600

    strings

---
+++ basics/Pypy_.py
@@ -0,0 +1,39 @@
+# x = int(input('inp1 '))
+# y = int(input('inp2 '))
+# z = int(input('inp3 '))
+# r_1 = x * y
+# r_2 = r_1 % z
+# print(r_2)
+
+# x = input('inp1 ')
+# y = input('inp2 ')
+# z = input('inp3 ')
+# r_1 = int(x) * int(y)
+# r_2 = r_1 % int(z)
+# print(r_2)
+
+# r_1 = int(input('inp1 ')) * int(input('inp2 '))
+# r_2 = r_1 % int(input('inp3 '))
+# print(r_2)
+
+
+# inp1 = int(input())
+# inp2 = int(input())
+# inp3 = int(input())
+# r_1 = inp1 * inp2
+# r_2 = r_1 % inp3
+# print(r_2)
+
+# radius = 5
+# import math
+# pi_ = round(math.pi, 2)
+# p =  int(pi_) * radius
+# s = int(pi_) * radius ** 2
+# print(p, s)
+
+# radius = 5
+# import math
+# pi_ = round(math.pi, 2)
+# p =  2 * pi_ * radius
+# s = pi_ * radius ** 2
+# print(p, s)
